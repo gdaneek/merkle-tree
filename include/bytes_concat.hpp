68ce7ee8f47a1ad9ac670070fe9f828a20ebc89e
gdaneek <daniladgubazov@gmail.com>
2025-05-27 04:02:53 +0300

    reset

--- include/bytes_concat.hpp
+++ include/bytes_concat.hpp
@@ -1,5 +1,5 @@
- 
-#pragma once
+#ifndef BYTES_CONCAT_LIB_HPP
+#define BYTES_CONCAT_LIB_HPP
 
 #include <utility>
 #include <array>
@@ -28,6 +28,8 @@ namespace bconcat {
     #include <iterator>
     #include <vector>
     #include <concepts>
+    #include <type_traits>
+
 
     template <typename T>
     concept Iterable = requires(T t) {
@@ -35,28 +37,34 @@ namespace bconcat {
         { std::end(t) } -> std::sentinel_for<decltype(std::begin(t))>;
     };
 
+
+    template<typename T>
+    concept MemcpyCopiable = requires(T t){
+        {std::is_trivially_copyable_v<T>};
+    };
+
     class UnifiedConcatenator {
     public:
-        using value_t = typename std::vector<char>;
+        using value_type = typename std::vector<char>;
 
         template<typename T> requires Iterable<T>
-        static void append(value_t& dst, T&& src) {
+        static void append(value_type& dst, T&& src)  {
             std::copy(std::begin(src), std::end(src), std::back_inserter(dst));
         }
 
-        template<typename T> requires (!Iterable<T>)
-        static void append(value_t& dst, T&& src) {
+
+        template<typename T> requires (!Iterable<T> && MemcpyCopiable<T>)
+        static void append(value_type& dst, T&& src) {
             auto curr_sz = dst.size();
             dst.resize(dst.size() + sizeof(src));
 
-            memcpy(dst.data()+curr_sz , &src, sizeof(src));
+            memcpy(dst.data() + curr_sz , &src, sizeof(src));
         }
-
 
 
         template<typename... Args>
         static auto concat(Args&&... args) {
-            value_t bytes;
+            value_type bytes{};
             ((append(bytes, std::forward<Args>(args))), ...);
 
             return bytes;
@@ -73,3 +81,5 @@ namespace bconcat {
     // TODO: DeepConcatenator
 }
 
+
+#endif // BYTES_CONCAT_LIB_HPP
